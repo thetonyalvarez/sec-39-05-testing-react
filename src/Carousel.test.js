61bdacddd014d10b28039d4bfbeebcd215fef9a9
Tony Alvarez <tonyalva87@gmail.com>
2022-07-12 21:43:09 -0700

    fix left arrow bug and write tests

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -11,6 +11,26 @@ it("matches snapshot", function() {
   expect(asFragment()).toMatchSnapshot();
 });
 
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move forward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+});
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
